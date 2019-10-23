ee3d479f0d6312b2c5ed37b0be05a9c97e29c8f7
Martin Zalabak <mana.zalabak@gmail.com>
2019-10-23 10:15:00 +0200

    indent four spaces

--- process.c
+++ process.c
@@ -14,65 +14,65 @@
  ******************************************************************************/
 int process_run(char* const cmd[], const bool cleanEnv)
 {
-  pid_t pid = process_spawn(cmd, cleanEnv, 0, 0);
-  if (pid < 0) { return pid; }
-  int wstatus = 0;
-  waitpid(pid, &wstatus, 0);
-  if (WIFEXITED(wstatus) > 0) {
-    return WEXITSTATUS(wstatus);
-  }
-  return -1;
+    pid_t pid = process_spawn(cmd, cleanEnv, 0, 0);
+    if (pid < 0) { return pid; }
+    int wstatus = 0;
+    waitpid(pid, &wstatus, 0);
+    if (WIFEXITED(wstatus) > 0) {
+        return WEXITSTATUS(wstatus);
+    }
+    return -1;
 }
 
 /*!*****************************************************************************
  * @see [process.h]
  ******************************************************************************/
 pid_t process_spawn(char* const cmd[], const bool cleanEnv,
-    int* stdinPipe, int* stdoutPipe)
+        int* stdinPipe, int* stdoutPipe)
 {
-  if (0 == cmd) { return -1; }
-  if (0 == *cmd) { return -1; }
+    if (0 == cmd) { return -1; }
+    if (0 == *cmd) { return -1; }
 
-  int stdinfd[2] = { 0 };
-  int stdoutfd[2] = { 0 };
-  if (0 != stdinPipe) {
-    if (-1 == pipe(stdinfd)) { return -2; }
-  }
-  if (0 != stdoutPipe) {
-    if (-1 == pipe(stdoutfd)) {
-      if (0 != stdoutPipe) { close(stdinfd[0]); close(stdinfd[1]); }
-      return -1;
-    }
-  }
-
-  pid_t pid = fork();
-  if (0 == pid)
-  {
+    int stdinfd[2] = { 0 };
+    int stdoutfd[2] = { 0 };
     if (0 != stdinPipe) {
-      if (-1 == dup2(stdinfd[0], 0)) { exit(errno); }
-      close(stdinfd[0]);
-      close(stdinfd[1]);
+        if (-1 == pipe(stdinfd)) { return -2; }
     }
     if (0 != stdoutPipe) {
-      if (-1 == dup2(stdoutfd[1], 1)) { exit(errno); }
-      close(stdoutfd[0]);
-      close(stdoutfd[1]);
+        if (-1 == pipe(stdoutfd)) {
+            if (0 != stdoutPipe) { close(stdinfd[0]); close(stdinfd[1]); }
+            return -1;
+        }
     }
-    if (true == cleanEnv) { clearenv(); }
-    execv(cmd[0], cmd);
-  }
 
-  if (0 != stdinPipe) {
-    close(stdinfd[0]);
-    if (pid > 0) { *stdinPipe = stdinfd[1]; }
-    else { close(stdinfd[1]); }
-  }
-  if (0 != stdoutPipe) {
-    close(stdoutfd[1]);
-    if (pid > 0) { *stdoutPipe = stdoutfd[0]; }
-    else { close(stdoutfd[0]); }
-  }
+    pid_t pid = fork();
+    if (0 == pid)
+    {
+        if (0 != stdinPipe) {
+            if (-1 == dup2(stdinfd[0], 0)) { exit(errno); }
+            close(stdinfd[0]);
+            close(stdinfd[1]);
+        }
+        if (0 != stdoutPipe) {
+            if (-1 == dup2(stdoutfd[1], 1)) { exit(errno); }
+            close(stdoutfd[0]);
+            close(stdoutfd[1]);
+        }
+        if (true == cleanEnv) { clearenv(); }
+        execv(cmd[0], cmd);
+    }
 
-  return pid;
+    if (0 != stdinPipe) {
+        close(stdinfd[0]);
+        if (pid > 0) { *stdinPipe = stdinfd[1]; }
+        else { close(stdinfd[1]); }
+    }
+    if (0 != stdoutPipe) {
+        close(stdoutfd[1]);
+        if (pid > 0) { *stdoutPipe = stdoutfd[0]; }
+        else { close(stdoutfd[0]); }
+    }
+
+    return pid;
 }
 
